a111fd969d19f019546554c878c9b949a039f04a
青灯 <qingdeng@local>
2026-08-08 23:13:29 +0800

    feat: 工作台布局 bento 重排 (v7) - 个人操作区 2 列非对称网格，本周动态卡内滚动

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // 个人工作台 Service Worker - 离线可开、可安装到主屏幕
-const CACHE = "workbench-v20";
+const CACHE = "workbench-v21";
 const FILES = [
   "./index.html",
-  "./styles.css?v=17",
+  "./styles.css?v=18",
   "./app.js?v=21",
   "./manifest.json",
   "./icon.svg"
