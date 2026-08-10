210badf821c9e3b1cb201702ca020cb2bd9ff2ac
青灯 <qingdeng@local>
2026-08-10 15:14:27 +0800

    周报项内容撑满整宽（名字左/时间右）+ 去掉滚动一次显示全部12条 + ⑤卡指标居中填充

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // 个人工作台 Service Worker - 离线可开、可安装到主屏幕
-const CACHE = "workbench-v46";
+const CACHE = "workbench-v47";
 const FILES = [
   "./index.html",
-  "./styles.css?v=38",
+  "./styles.css?v=39",
   "./app.js?v=44",
   "./manifest.json",
   "./icon.svg"
