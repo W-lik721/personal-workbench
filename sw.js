bc1bd41133f5ac008093d7c11425daa9376fafaa
青灯 <qingdeng@local>
2026-08-10 15:52:26 +0800

    本周动态卡片也改两列（与tab内容统一）

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // 个人工作台 Service Worker - 离线可开、可安装到主屏幕
-const CACHE = "workbench-v52";
+const CACHE = "workbench-v53";
 const FILES = [
   "./index.html",
-  "./styles.css?v=44",
+  "./styles.css?v=45",
   "./app.js?v=46",
   "./manifest.json",
   "./icon.svg"
